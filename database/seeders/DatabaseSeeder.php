18f421ad16b7e884908c1d572504e7d81becc123
NawLarPhawHtoo <113975423+NawLarPhawHtoo@users.noreply.github.com>
2023-10-18 11:12:27 +0630

    #add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +17,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = [
+            [
+              'name' => 'Admin',
+              'email' => 'admin@gmail.com',
+              'password' => Hash::make('123456'),
+              'profile' => '',
+              'type' => '0',
+              'created_user_id' => 1,
+              'updated_user_id' => 1,
+            ],
+            [
+              'name' => 'User',
+              'email' => 'user@gmail.com',
+              'password' => Hash::make('123456'),
+              'profile' => '',
+              'type' => '1',
+              'created_user_id' => 2,
+              'updated_user_id' => 2,
+            ],
+          ];
+          foreach ($user as $key => $value) {
+            User::create($value);
+          }
     }
 }
