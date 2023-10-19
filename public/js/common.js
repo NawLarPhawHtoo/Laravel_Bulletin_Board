dd543328a497ad7737f51e889ca2c33c8b582259
NawLarPhawHtoo <113975423+NawLarPhawHtoo@users.noreply.github.com>
2023-10-19 09:44:24 +0630

    #change data future

--- public/js/common.js
+++ public/js/common.js
@@ -24,5 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-var today = new Date().toISOString().split('T')[0];
-document.getElementById("dob").setAttribute("max", today);
+document.addEventListener("DOMContentLoaded", function() {
+  var today = new Date().toISOString().split('T')[0];
+  document.getElementById("dob").setAttribute("max", today);
+});
